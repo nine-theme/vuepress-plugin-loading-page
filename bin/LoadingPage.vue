4208551aebbe2426304293d227933822f87ec6ca
NineSwordsMonster <wangjia_1919@163.com>
2020-08-27 20:03:05 +0800

    update loading style

--- bin/LoadingPage.vue
+++ bin/LoadingPage.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="loader-wrapper">
     <div class="loader-main">
-      <div v-for="item in 4" :key="`out${item}`">
-      </div>
+      <span class="l-1"></span>
+      <span class="l-2"></span>
+      <span class="l-3"></span>
+      <span class="l-4"></span>
+      <span class="l-5"></span>
+      <span class="l-6"></span>
     </div>
     <h3 class="title" v-if="$frontmatter.home">{{$site.title || $localeConfig.title}}</h3>
     <p class="description" v-if="$frontmatter.home">{{$site.description || $localeConfig.description}}</p>
@@ -23,42 +27,57 @@
     left:50%;
     z-index:555;
     transform:translate(-50%,0);
-    div
-      &:nth-child(2){ animation:pacman-balls 1s 0s infinite linear; }
-      &:nth-child(3){ animation:pacman-balls 1s 0.33s infinite linear; }
-      &:nth-child(4){ animation:pacman-balls 1s 0.66s infinite linear; }
-      &:nth-child(5){ animation:pacman-balls 1s 0.99s infinite linear; }
-      &:first-of-type
-        width:0px;
-        height:0px;
-        border-right:25px solid transparent;
-        border-top:25px solid $accentColor;
-        border-left:25px solid $accentColor;
-        border-bottom:25px solid $accentColor;
-        border-radius:25px;
-        animation:rotate_pacman_half_up 0.5s 0s infinite;
-      &:nth-child(2)
-        width:0px;
-        height:0px;
-        border-right:25px solid transparent;
-        border-top:25px solid $accentColor;
-        border-left:25px solid $accentColor;
-        border-bottom:25px solid $accentColor;
-        border-radius:25px;
-        animation:rotate_pacman_half_down 0.5s 0s infinite;
-        margin-top:-50px;
-      &:nth-child(3),&:nth-child(4),&:nth-child(5),&:nth-child(6)
-        background-color:$accentColor;
-        width:15px;
-        height:15px;
-        border-radius:100%;
-        margin:2px;
-        width:10px;
-        height:10px;
-        position:absolute;
-        transform:translate(0,-6.25px);
-        top:25px;
-        left:100px;
+    span[class*="l-"] {
+      height: 24px; width: 34px;
+      background: #3eaf7c;
+      display: inline-block;
+      margin: 12px 2px;
+      border-radius: 100%;
+      -webkit-border-radius: 100%;
+      -moz-border-radius: 100%;
+      -webkit-animation: loader 4s infinite;
+      -webkit-animation-timing-function: cubic-bezier(0.030, 0.615, 0.995, 0.415);
+      -webkit-animation-fill-mode: both;
+      -moz-animation: loader 4s infinite;
+      -moz-animation-timing-function: cubic-bezier(0.030, 0.615, 0.995, 0.415);
+      -moz-animation-fill-mode: both;
+      -ms-animation: loader 4s infinite;
+      -ms-animation-timing-function: cubic-bezier(0.030, 0.615, 0.995, 0.415);
+      -ms-animation-fill-mode: both;
+      animation: loader 4s infinite;
+      animation-timing-function: cubic-bezier(0.030, 0.615, 0.995, 0.415);
+      animation-fill-mode: both;
+    }
+    span.l-1 {-webkit-animation-delay: 1s;animation-delay: 1s;-ms-animation-delay: 1s;-moz-animation-delay: 1s;}
+    span.l-2 {-webkit-animation-delay: 0.8s;animation-delay: 0.8s;-ms-animation-delay: 0.8s;-moz-animation-delay: 0.8s;}
+    span.l-3 {-webkit-animation-delay: 0.6s;animation-delay: 0.6s;-ms-animation-delay: 0.6s;-moz-animation-delay: 0.6s;}
+    span.l-4 {-webkit-animation-delay: 0.4s;animation-delay: 0.4s;-ms-animation-delay: 0.4s;-moz-animation-delay: 0.4s;}
+    span.l-5 {-webkit-animation-delay: 0.2s;animation-delay: 0.2s;-ms-animation-delay: 0.2s;-moz-animation-delay: 0.2s;}
+    span.l-6 {-webkit-animation-delay: 0;animation-delay: 0;-ms-animation-delay: 0;-moz-animation-delay: 0;}
+    @-webkit-keyframes loader {
+      0% {-webkit-transform: translateX(-30px); opacity: 0;}
+      25% {opacity: 1;}
+      50% {-webkit-transform: translateX(30px); opacity: 0;}
+      100% {opacity: 0;}
+    }
+    @-moz-keyframes loader {
+      0% {-moz-transform: translateX(-30px); opacity: 0;}
+      25% {opacity: 1;}
+      50% {-moz-transform: translateX(30px); opacity: 0;}
+      100% {opacity: 0;}
+    }
+    @-keyframes loader {
+      0% {-transform: translateX(-30px); opacity: 0;}
+      25% {opacity: 1;}
+      50% {-transform: translateX(30px); opacity: 0;}
+      100% {opacity: 0;}
+    }
+    @-ms-keyframes loader {
+      0% {-ms-transform: translateX(-30px); opacity: 0;}
+      25% {opacity: 1;}
+      50% {-ms-transform: translateX(30px); opacity: 0;}
+      100% {opacity: 0;}
+    }
   .title
     margin 8rem auto 2rem
     text-align center
@@ -77,15 +96,4 @@
     box-sizing: border-box;
     padding: 0 10px;
     text-shadow 0 2px 10px rgba(0,0,0,0.2);
-  @keyframes pacman-balls
-    75%{opacity:0.7;}
-    100%{-webkit-transform:translate(-100px,-6.25px);transform:translate(-100px,-6.25px);}
-  @keyframes rotate_pacman_half_up
-    0%{-webkit-transform:rotate(270deg);transform:rotate(270deg);}
-    50%{-webkit-transform:rotate(360deg);transform:rotate(360deg);}
-    100%{-webkit-transform:rotate(270deg);transform:rotate(270deg);}
-  @keyframes rotate_pacman_half_down
-    0%{-webkit-transform:rotate(90deg);transform:rotate(90deg);}
-    50%{-webkit-transform:rotate(0deg);transform:rotate(0deg);}
-    100%{-webkit-transform:rotate(90deg);transform:rotate(90deg);}
 </style>
